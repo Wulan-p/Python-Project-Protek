c09555a402995123f9679baf19b886babbb5741d
Wulan-p <72066844+Wulan-p@users.noreply.github.com>
2021-01-13 00:43:39 +0700

    Add files via upload

---
+++ Chapter 11/Phyton Project 1.py
@@ -0,0 +1,31 @@
+
+from datetime import *
+
+def diffDate(x) :
+    tanggal = x.split("-")
+    dateList = []
+
+    for i in tanggal :
+        dateList.append(int(i))
+
+    past = date(dateList[0], dateList[1], dateList[2])
+    now = datetime.date(datetime.now())
+
+    hasil = past - now
+    hasil1 = (hasil.days)
+
+    return hasil1
+
+'''
+#mencoba function
+print(diffDate('2018-12-30'))
+print(diffDate('2021-01-20'))
+try :
+    ans = input("Masukkan tanggal (yyyy-mm-dd) : ")
+    hasil = diffDate(ans)
+    print("Selisih tanggal {0} dengan hari ini adalah {1} hari".format(ans, hasil))
+    print ("\nnb. (-) memiliki arti hari sudah lampau")
+except ValueError :
+    print("Input Invalid")
+'''
+
---
+++ Chapter 11/Phyton Project 2.py
@@ -0,0 +1,33 @@
+import os
+from datetime import *
+
+if(os.path.exists) :
+        mode = 'a'
+
+else :
+        mode = 'w'
+
+file = open('DataPeminjaman.txt', mode)
+
+while True :  
+    kode = input("\nMasukkan Kode Member : ")
+    nama = input("Masukkan Nama Member : ")
+    judul = input("Masukkan Judul Buku  : ")
+
+    pinjam = date.today()
+    kembali = pinjam + timedelta(days = 7)
+
+    file.write(kode + '|' + nama + '|' + judul + '|' + str(pinjam) + '|' + str(kembali) + '\n')
+
+    ulang = input("\nUlangi lagi (y/n)    : ")
+    if (ulang == 'Y' or ulang == 'y') : 
+        continue
+
+    elif (ulang == 'N' or ulang == 'n') : 
+        break
+
+    else :
+        print("Input Ivalid")
+        break
+
+file.close()
